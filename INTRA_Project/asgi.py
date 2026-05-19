1269f2bcd0af9490a6946ccc0d0f7cce3714decf
Aoibhe_Hegarty <a.hegarty004@gmai.com>
2026-05-19 18:58:11 +0100

    added index.html and linked urls correctly

--- INTRA_Project/asgi.py
+++ INTRA_Project/asgi.py
@@ -10,14 +10,7 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from django.contrib import admin
-from django.urls import path, include
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'INTRA_Project.settings')
 
 application = get_asgi_application()
-
-urlpatterns = [
-    path('', include('Good_Health_Wellbeing.urls')),
-    path('admin/', admin.site.urls),
-]

6f251d687ba380c8be8d68039461bcaf7405ac2f
Aoibhe_Hegarty <a.hegarty004@gmai.com>
2026-05-20 10:42:06 +0100

    added ingredient model

--- Good_Health_Wellbeing/models.py
+++ Good_Health_Wellbeing/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# model of end user
 class User(models.Model):
+    id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=50)
 
     def __str__(self):
         return self.username
+    
+class Ingredient(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField()
+
+    def __str__(self):
+        return self.name
--- Good_Health_Wellbeing/views.py
+++ Good_Health_Wellbeing/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+#page to view list of recipies
+def view_recipe_list(request):
+    all_recipies = 

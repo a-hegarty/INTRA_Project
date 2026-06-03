22e1595074b115f5321b72d15b7676a49154a3b9
Aoibhe_Hegarty <a.hegarty004@gmai.com>
2026-06-03 18:47:23 +0100

    added category to ingredients model

--- Good_Health_Wellbeing/models.py
+++ Good_Health_Wellbeing/models.py
@@ -8,9 +8,17 @@ class User(models.Model):
     def __str__(self):
         return self.username
     
+class IngredientCategory(models.Model):
+    id = models.AutoField(primary_key=True),
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
 class Ingredient(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, default='')
+    category = models.ForeignKey(IngredientCategory, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name

dc089cfef11a22c21cadea6c8ead1c3b237b9cd8
Aoibhe_Hegarty <a.hegarty004@gmai.com>
2026-06-03 19:10:47 +0100

    added model ingredient Category

--- Good_Health_Wellbeing/models.py
+++ Good_Health_Wellbeing/models.py
@@ -8,6 +8,13 @@ class User(models.Model):
     def __str__(self):
         return self.username
     
+class IngredientCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, default='')
+
+    def __str__(self):
+        return self.name
+    
 class Ingredient(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, default='')
